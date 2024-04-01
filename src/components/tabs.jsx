2a1cfcda57458767ffbc78645424e08718a7050a
Sina Persian <sinasasy4@gmail.com>
2024-04-01 13:19:47 -0700

    Custom Tabs

---
+++ src/components/tabs.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+const Tabs = ({ tabContent, onChange }) => {
+  const [currentTabIndex, setCurrentTabIndex] = useState(0);
+  function handleOnClick(getCurrentIndex){
+    setCurrentTabIndex(getCurrentIndex)
+    onChange(getCurrentIndex)
+  }
+
+  return (
+    <div className="wrapper">
+      <div className="heading">
+        {tabContent.map((tabItem,index) => (
+          <div className={`tab-item ${currentTabIndex === index ? 'active'  : ''}`} onClick={()=>handleOnClick(index)} key={tabItem.label}>
+            <span className="label">{tabItem.label}</span>
+          </div>
+        ))}
+      </div>
+      <div className="content" style={{color :'red'}}>
+        {tabContent[currentTabIndex] && tabContent[currentTabIndex].content}
+      </div>
+    </div>
+  );
+};
+
+export default Tabs;
